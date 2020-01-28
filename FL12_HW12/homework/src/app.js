04dc4f87902d87450515860f4a7fbe49b0c8c439
heletrix <havrilyuk.olena@gmail.com>
2020-01-28 23:57:45 +0200

    hw12 html content

--- FL12_HW12/homework/src/app.js
+++ FL12_HW12/homework/src/app.js
@@ -1,4 +1,8 @@
 
-
-// Your code goes here
-
+ window.onload = function() {
+    document.getElementById('addSetButton').onclick = function() {
+        document.getElementById('main-page').style.display === 'none';
+        document.getElementById('modify-set-page').style.display === 'none';
+        document.getElementById('add-set-page').style.display = 'block';
+    };
+}
